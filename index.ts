98a7b6b18dca8b8d2e111ef457fa67019bfea466
avraham <2721avi@gmail.com>
2024-09-26 10:17:10 +0300

    endpoints defined

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("hi");
+import express, { Express } from "express";
+import "dotenv/config";
+import v4 from "uuid";
+
+const app: Express = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`See you at http::localhost:${process.env.PORT}`);
+});
+console.log(process.env.PORT);
